1b235545db98c7924c9c5b70bcbef7f4f2f62c6f
HongxuHuang <hongxuhu@buffalo>
2024-03-09 20:04:55 -0500

    Revert "JS file"
    This reverts commit 291cbf7dac0eab59671afc73693dc76f645e9092.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, make_response, url_for, render_template
+from flask import Flask
 def create_app():
 
     app = Flask(__name__)
@@ -14,11 +14,6 @@ def create_app():
         return Flask.response_class(b'hello',status= 200,headers=headers,mimetype= "text/plain")
     return app
 
-    @app.route('/js')
-    def js_page():
-        return render_template('js.html')
-
-    return app
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True,host = "0.0.0.0", port = 8080)
