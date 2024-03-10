1276cc2b7341534b471b53ccfcba8de6efaaa622
HongxuHuang <hongxuhu@buffalo>
2024-03-09 20:16:00 -0500

    CSS file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 def create_app():
 
     app = Flask(__name__)
@@ -12,7 +14,17 @@ def create_app():
         }
       #sending example response from the root page
         return Flask.response_class(b'hello',status= 200,headers=headers,mimetype= "text/plain")
+
+    @app.route('/css')
+    def css_page():
+        headers = {
+            "X-Content-Type-Options": "nosniff",
+            "Content-Length": "5"
+        }
+
+        return render_template('css.html', headers=headers)
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
