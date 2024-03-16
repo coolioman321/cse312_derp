1661bb107e2225068ac9b9a10cfdac2a9399baa1
andrewwong <aqwong@buffalo.edu>
2024-03-15 22:55:58 -0400

    implement backend functionality for registration

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, make_response, render_template, send_from_directory
+from flask import Flask, make_response, render_template, send_from_directory, request, redirect, url_for
+from pymongo import MongoClient
+from util.helper import validate_password, escape_html
+import bcrypt
+
+mongo_client = MongoClient('mongo')
+db = mongo_client['derp']
+users = db['users']
 
 def create_app():
     app = Flask(__name__)
@@ -31,6 +38,34 @@ def create_app():
         response = send_from_directory('.', 'app.js', mimetype='text/javascript')
         response.headers["X-Content-Type-Options"] = "nosniff"
         return response
+
+    # Serve the registration page
+    @app.route('/register', methods=['POST'])
+    def register():
+        username = request.form['username']
+        password = request.form['password']
+        confirmPassword = request.form['confirmPassword']
+        
+        # Validate password
+        if not validate_password(password):
+            return redirect(url_for('home_page'))
+        
+        # Check if password and confirm password match
+        if password != confirmPassword:
+            return redirect(url_for('home_page'))
+        
+        # Check if username is already taken
+        if users.find_one({"username": username}):
+            return redirect(url_for('home_page'))
+        
+        # Salt and hash the password
+        encrypted_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        
+        # Insert the user into the database
+        users.insert_one({"username": username, "password": encrypted_pw})
+        
+        # Redirect back to homepage
+        return redirect(url_for('home_page'))
 
     return app
 
